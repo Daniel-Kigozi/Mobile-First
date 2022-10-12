7b48759b21d56ebbb773d010fc93f04a3c529ef7
Daniel-Kigozi <kdanalex69@gmail.com>
2022-10-12 22:17:03 +0300

    add desktop popup

--- mobile.js
+++ mobile.js
@@ -1,73 +1,101 @@
+
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navMenu = document.querySelector('.nav-menu');
 const body = document.querySelector('body');
 const popup = document.querySelector('.button-line');
-const popupbody = document.querySelector('.popup-body')
+const popupbody = document.querySelector('.popup-body');
+const projectWork = document.querySelector('.main-works');
+
 hamburgerMenu.addEventListener('click', () => {
   hamburgerMenu.classList.toggle('active');
   navMenu.classList.toggle('active');
-});
+});popup-body
 document.querySelectorAll('.nav-link').forEach((navLink) => navLink.addEventListener('click', () => {
   hamburgerMenu.classList.remove('active');
   navMenu.classList.remove('active');
 }));
 
-popup.addEventListener('click', ()=>{
-  let div = document.createElement('div');
-  div.className='elementpop';
-  div.innerHTML = `
+const displayProjectDetails = ({ LiveLink, SourceLink, description, image, technologies, name })=>{
   
-  <a class="close-button" id="close-btn" type="button" href="#">X</a>
-  <div class="popup-img"><img src="media/Snapshoot Portfolio.png"/></div>
-  <div class="popup-data">
-      <p class="pop-para">Keeping track of hundreds of components</p>
-      <ul class="content-inner-pop">
-          <li class="pop-btn"> <a class="button-pop" type="button" href="#">Ruby on rails</a></li>
-          <li class="pop-btn"> <a class="button-pop" type="button" href="#">Css</a></li>
-          <li class="pop-btn"> <a class="button-pop" type="button" href="#">Javascript</a></li>
-      </ul>
-  </div>
-  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem</p>
-  <div>
-      <a class="button button-line" type="button" href="#">See Live <span><img src="media/github.svg"/></span></a>
-      <a class="button-pop button-line " type="button" href="#">See Source<img id="inner-pop" src="media/github.svg"></a> 
-  </div>
+    let div = document.createElement('div');
+    div.className='elementpop';
+    div.innerHTML = `
+    <div class=inner-pop-btn>
+    <a class="close-button" id="close-btn" type="button" href="">X</a>
+    <div class="popup-img"><img src="${image}"/></div>
+    </div>
+    <div class="popup-data">
+        <p class="pop-para">${name}</p>
+        <ul class="content-inner-pop">
+            <li class="pop-btn"> <a class="button-pop" type="button" href="">${technologies[0]}</a></li>
+            <li class="pop-btn"> <a class="button-pop" type="button" href="">${technologies[2]}</a></li>
+            <li class="pop-btn"> <a class="button-pop" type="button" href="">${technologies[3]}</a></li>
+        </ul>
+    </div>
+    <p class="pop-desc">${description}</p>
+    <div class=main-works-pop>
+        <a class="button button-line" type="button" href="${LiveLink}">See Live <span><img src="media/github.svg"/></span></a>
+        <a class="button button-line" type="button" href="${SourceLink}">See Source <span><img id="img-pop" src="media/github.svg"/></span></a>  
+    </div>
+  
+    `
+    return div;
+  
+}
 
-  `
-  popupbody.append(div);
-  popupbody.classList.add('showpopup');
-})
+const displayProjects = ({name, technologies, client}, index)=>{
+  let li = document.createElement('li');
+  li.className ="list-item";
+  li.innerHTML = `
+          <div class="first-item">
+              <div class="first-item-inner">
+                  <h3 class="title-sub"><span>${name}</span><span>${client}</span></h3>
+                  <ul class="content-inner">
+                      <li class="content-inner-li">${technologies[0]}</li>
+                      <li class="content-inner-li">${technologies[1]}</li>
+                      <li class="content-inner-li">${technologies[2]}</li>
+                      <li class="content-inner-li">${technologies[3]}</li>
+                  </ul>
+                  <a id="${index}" class="button button-line"  type="button" href="">See Project</a>
+              </div>
+          </diV>
+        `;
+        return li;
+}
 
+const getProjects = async()=>{
+  const response = await fetch('./projects.json');
+  try {
+    const data = await response.json();
+    data.forEach((project, index) =>{
+      projectWork.append(displayProjects(project, index))
+    })
+  } catch (error) {
+    
+  }
+  
+  
+}
 
+window.onload = async ()=>{
+  document.querySelectorAll('.button-line').forEach((button)=> button.addEventListener('click', async(e)=> {
+    e.preventDefault();
+    const response = await fetch('./projects.json');
+    const data = await response.json();
+    let itemId = e.srcElement.attributes.id.value;
+    console.log(data[itemId], itemId);
+    popupbody.append(displayProjectDetails(data[itemId]));
+    popupbody.classList.add('showpopup');
 
+    const closeBtn = document.querySelector('#close-btn');
+    closeBtn.addEventListener('click', (e)=>{
+      e.preventDefault();
+    popupbody.classList.remove('showpopup');
+    popupbody.innerHTML = "";
+  })
+    
+  }))
+  
+}
 
-// popup.addEventListener('click', ()=>{
-//   const element = document.createElement('section');
-//   element.className ='element'
-//   const elementPop = document.createElement('div');
-//   elementPop.className = 'elementpop'
-//   element.appendChild(elementPop);
-//   elementPop.innerHTML = `
-//   <a class="close-button" id="close-btn" type="button" href="#">X</a>
-//   <div class="popup-data">
-//       <p class="pop-para">Keeping track of hundreds of components</p>
-//       <ul class="content-inner-pop">
-//           <li class="pop-btn"> <a class="button-pop" type="button" href="#">Ruby on rails</a></li>
-//           <li class="pop-btn"> <a class="button-pop" type="button" href="#">Css</a></li>
-//           <li class="pop-btn"> <a class="button-pop" type="button" href="#">Javascript</a></li>
-//       </ul>
-//   </div>
-//   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem</p>
-//   <div>
-//       <a class="button button-line" type="button" href="#">See Live</a>
-//       <a class="button-pop button-line " type="button" href="#">See Source<img id="inner-pop" src="media/github.svg"></a> 
-//   </div>
-//   `;
-//   body.appendChild(element);
-
-//   const closeButton= document.querySelector('#close-btn');
-//   closeButton.addEventListener('click', ()=>{
-//     body.removeChild(element);
-//   })
-
-// })
+getProjects();
